938ed26a1393a869f5a9f91f354473d6b80d4d78
Anthony Lam <thelamacmdr@gmail.com>
2016-03-04 23:38:59 -0800

    Update

--- emit_nvim.sh
+++ emit_nvim.sh
@@ -2,7 +2,7 @@
 
 dir=~/dotfiles
 backup_dir=~/dotfiles/backup
-files="bashrc vimrc Xresources"
+files="bash_it tmux.conf bashrc vimrc Xresources"
 
 # Backup previous files into ./backup folder
 echo "Creating $backup_dir for backup of existing dotfiles"
