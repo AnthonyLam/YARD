588aa947b545ee7107fcd1ae703e4261dd1931a9
Anthony Lam <thelamacmdr@gmail.com>
2016-04-01 20:25:00 -0700

    Add Go stuff

--- bash_it/custom/exports.bash
+++ bash_it/custom/exports.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PYTHONPATH="/home/anthony/Dropbox/My Documents/Projects/think_bayes/ThinkBayes/code/":$PYTHONPATH
-export GOPATH="/home/anthony/Documents/go":$GOPATH
+export GOPATH="/home/anthony/Documents/go/"
